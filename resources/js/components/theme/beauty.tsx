2d5d9c224a897a5282bdcab2e5d26bf3b2bd20af
Favour Shaw <favourvanderwaal@gmail.com>
2025-06-15 22:00:27 +0100

    fix: remove 'Blog' link from navigation in Beauty component

--- resources/js/components/theme/beauty.tsx
+++ resources/js/components/theme/beauty.tsx
@@ -46,7 +46,6 @@ export default function Beauty({ profileData }: UsersPgProps) {
         { label: 'Home', href: '#home' },
         { label: 'About', href: '#about' },
         { label: 'Services', href: '#services' },
-        { label: 'Blog', href: '#blog' },
         { label: 'Contact', href: '#contact' },
     ];
 
